c692b4cbb7e2a267f1cb45a62068bac4de2bd23c
Adam Carter <adamrcarter@gmail.com>
2014-08-25 13:00:39 -0500

    added info to db with Ruby

--- main.rb
+++ main.rb
@@ -15,9 +15,9 @@ ForecastIO.api_key = 'bc799f9c215bbe4bd9f3fc497bfde58b'
 @forecast = ForecastIO.forecast(41.2586100, -95.9377900) #omaha coordinates
 
 # deleted the other 3 entries from here, leaving this as example for how to add more entries into pry if needed
-# #$u1 = User.create({name: "Adam"})
+$u1 = User.create({name: "Adam"})
 #
-# $t1 = Trail.create({trail_name: "Zorinsky Park", lat: 41.218889, long: -96.167222})
+ $t1 = Trail.create({trail_name: "Zorinsky Park"})
 #
-# $r1 = Review.create({content: "Around a lake; mainly flat with some hills; gets busy on weekends.", user_id: 1, trail_id: 1})
+ $r1 = Review.create({content: "Around a lake; mainly flat with some hills; gets busy on weekends.", user_id: 1, trail_id: 1})
 
